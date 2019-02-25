80398f287f43d8b2e2d08618ff602327802de55a
Pavel Tikhachev <p.tihachev@uiscom.ru>
2019-02-25 17:59:28 +0300

    fir logging errors

--- start37.py
+++ start37.py
@@ -6,30 +6,30 @@ from logging import config, getLogger
 from vk_dice_roll.lina import Lina
 from vk_dice_roll.core.utils import create_logger
 
+if 'HEROKU_APP' in os.environ:
+    app_id = os.environ.get('APP_ID')
+    login = os.environ.get('LOGIN', '')
+    password = os.environ.get('PASSWORD', '')
+    secret_key = os.environ.get('SECRET_KEY')
+    admin_id = int(os.environ.get('ADMIN_KEY'))
+    logger = create_logger()
+else:
+    import local_settings
+
+    app_id = local_settings.APP_ID
+    login = local_settings.LOGIN
+    password = local_settings.PASSWORD
+    secret_key = local_settings.SECRET_KEY
+    admin_id = local_settings.ADMIN_KEY
+    if hasattr(local_settings, 'LOG_CONFIG'):
+        log_config = local_settings.LOG_CONFIG
+        config.dictConfig(log_config)
+        logger = getLogger('lina')
+    else:
+        logger = create_logger()
+
 
 async def main():
-    if 'HEROKU_APP' in os.environ:
-        app_id = os.environ.get('APP_ID')
-        login = os.environ.get('LOGIN', '')
-        password = os.environ.get('PASSWORD', '')
-        secret_key = os.environ.get('SECRET_KEY')
-        admin_id = int(os.environ.get('ADMIN_KEY'))
-        logger = create_logger()
-    else:
-        import local_settings
-        app_id = local_settings.APP_ID
-        login = local_settings.LOGIN
-        password = local_settings.PASSWORD
-        secret_key = local_settings.SECRET_KEY
-        admin_id = local_settings.ADMIN_KEY
-        if hasattr(local_settings, 'LOG_CONFIG'):
-            log_config = local_settings.LOG_CONFIG
-            config.dictConfig(log_config)
-            logger = getLogger('lina')
-        else:
-            logger = create_logger()
-
-
     lina = Lina(app_id=app_id,
                 login=login,
                 password=password,
@@ -37,9 +37,11 @@ async def main():
                 secret_key=secret_key,
                 names=('моли', 'молли'),
                 logger=logger)
-
     await lina.start()
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except (Exception, KeyboardInterrupt) as e:
+        logger.error('', exc_info=True)
